4f1364babd9900fdaef689233917a5f93ce21508
Vincy King <44465544+ICY2INVINCIBLE@users.noreply.github.com>
2021-03-12 17:06:56 +0800

    Add files via upload

---
+++ static/js/test.js
@@ -0,0 +1,35 @@
+
+var nodemailer = require('Web/KidFocus/static/js/nodemailer');
+var transporter = nodemailer.createTransport({
+    //https://github.com/andris9/nodemailer-wellknown#supported-services 支持列表
+    service: 'qq',
+    port: 465, // SMTP 端口
+    secureConnection: true, // 使用 SSL
+    auth: {
+        user: '1032505669@qq.com',
+        //这里密码不是qq密码，是你设置的smtp密码
+        pass: 'vsbpdlrtrtucbfcf'
+    }
+});
+
+// NB! No need to recreate the transporter object. You can use
+// the same transporter object for all e-mails
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: '1032505669@qq.com', // 发件地址
+    to: '1032505669@qq.com', // 收件列表
+    subject: 'Hello sir', // 标题
+    //text和html两者只支持一种
+    text: 'Hello world ?', // 标题
+    html: '<b>Hello world ?</b>' // html 内容
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+
+});
